1d5ad9413c0dfbc0178aa32411705625cccf352d
Guru Senthil <guru.koushik@gmail.com>
2023-10-06 14:32:01 -0700

    fix: remove resume page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -25,10 +25,6 @@ export default function Home() {
       title: "Blogs",
       link: "/Blogs",
     },
-    {
-      title: "Resume",
-      link: "/Resume",
-    },
   ];
   const social = [
     {
